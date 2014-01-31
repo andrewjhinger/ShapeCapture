c2a4c275727f46fd35c8581652cad891f42dfdf4
Andrew Hinger <andrewjhinger@gmail.com>
2014-01-31 09:57:51 -0500

    Update ICollector.cs

--- ShapeCapture/ICollector.cs
+++ ShapeCapture/ICollector.cs
@@ -8,10 +8,6 @@ namespace ShapeCapture
 {
     public interface ICollector
     {
-        /*AH The following properties and methods belong to the Interface and must be implemented.
-        includes properties that tabulate the number of shapes that have been collected, 
-        the total number of points from items that have been collected, 
-        a Collect method to enable the Collector to collect a shape, and a method to reset the Collector. */
         Size Dimensions { get; set; }
         Point Location { get; set; }
         Color FillColor { get; set; }
